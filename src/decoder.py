133ebe23d32584ba788cbe90057087a7c1b15ad7
Anass Sindab <anass.sindab.1337@gmail.com>
2026-07-29 20:13:31 +0100

    working on tokenizer wrapper

--- src/decoder.py
+++ src/decoder.py
@@ -1,2 +1,22 @@
 from grammar import Grammar
-from llm_sdk import 
+from llm_sdk import Small_LLM_Model
+
+class Tokenizer:
+    def __init__(self):
+        self.model = Small_LLM_Model()
+
+    def encode(self, text):
+        return self.model.encode(text)
+
+
+    def decode(self, ids):
+        return self.model.decode(ids)
+
+    def token_to_id(self, token):
+        return self.model.encode(token).squeeze().tolist()
+
+    def get_logits(self, ids):
+        return self.model.get_logits_from_input_ids(ids)
+
+    def vocab_file_path(self):
+        return self.model.get_path_to_vocab_file()
